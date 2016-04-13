018b9381b088c04ade9446b8ae18a8b54407f09f
Fabian Schuiki <fabian@schuiki.ch>
2016-04-13 23:10:59 +0200

    Make install.sh mkdir ~/.vim and clone Vundle

--- install.sh
+++ install.sh
@@ -6,6 +6,16 @@ link() {
 	fi
 }
 
+if [ ! -d ~/.vim ]; then
+	echo "- Making Vim directory"
+	mkdir -p ~/.vim
+fi
+
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+	echo "- Installing Vundle"
+	git clone -b v0.10.2 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
 DOTFILES=~/.dotfiles
 link $DOTFILES/vimrc ~/.vimrc
 link $DOTFILES/config/fish/functions ~/.config/fish/functions
