1b53c3c9ac02d2c0210c2ac3637647d83aafb6cb
Fabian Schuiki <fabian@schuiki.ch>
2016-04-23 17:07:46 +0200

    Make install script work on Mac OS X

--- install.sh
+++ install.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
+# Copyright (c) 2016 Fabian Schuiki
+# Installs the dotfiles on the system.
 
 link() {
 	FLAGS=
-	if [ ! -L $2 ] && [ ! -e $2 ]; then
+	if [ ! -L "$2" ] && [ ! -e "$2" ]; then
 		echo "-- Linking $2"
 		FLAGS=-s
-	elif [ $(readlink $2) != "$1" ]; then
+	elif [ "$(readlink "$2")" != "$1" ]; then
 		echo "-- Linking $2"
 		read -p "   already exists, overwrite? [y/n] " -n 1 -r
 		echo "  "
 		if [[ $REPLY =~ ^[Yy]$ ]]; then
-			FLAGS=-sf
+			if [ "$(uname)" == "Darwin" ]; then
+				FLAGS=-sFf
+			else
+				FLAGS=-sf
+			fi
 		else
 			echo "   skipped"
 		fi
 	fi
 	if [ ! -z $FLAGS ]; then
-		ln -T $FLAGS $1 $2
+		if [ "$(uname)" == "Darwin" ]; then
+			ln -h $FLAGS "$1" "$2"
+		else
+			ln -T $FLAGS "$1" "$2"
+		fi
 	fi
 }
 
@@ -34,10 +44,22 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
 	popd
 fi
 
-DOTFILES=$(readlink -m $(dirname $BASH_SOURCE))
+if [ "$(uname)" == "Darwin" ]; then
+	DOTFILES="$PWD/$(dirname $BASH_SOURCE)"
+else
+	DOTFILES=$(readlink -m "$(dirname $BASH_SOURCE)")
+fi
 link $DOTFILES/vimrc ~/.vimrc
 link $DOTFILES/config/fish/ ~/.config/fish
 link $DOTFILES/tmux/tmux.conf.symlink ~/.tmux.conf
 
-# if [ "$(uname)" == "Darwin" ]; then
-# fi
+SUBL_SRC=$DOTFILES/config/sublime-text-3/Packages/User
+if [ "$(uname)" == "Darwin" ]; then
+	SUBL_DST=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+else
+	SUBL_DST=~/.config/sublime-text-3/Packages/User
+fi
+
+link "$SUBL_SRC/Preferences.sublime-settings" "$SUBL_DST/Preferences.sublime-settings"
+link "$SUBL_SRC/Markdown.sublime-settings" "$SUBL_DST/Markdown.sublime-settings"
+link "$SUBL_SRC/Diff.sublime-settings" "$SUBL_DST/Diff.sublime-settings"
